ce228619e28028eaf0aa2ab29a371e5467302f9b
yen Lin <erte00000@gmail.com>
2023-08-08 23:47:43 +0800

    first

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import FirstComponent from "./Components/FirstComponent";
 
 function App() {
   return (
-    <div className="">
+    <div>
       <FirstComponent />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
