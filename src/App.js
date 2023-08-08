04a34fb6be1d25d780154bcf6d5c832a0f38a5b6
yen Lin <erte00000@gmail.com>
2023-08-08 23:42:16 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import FirstComponent from "./Components/FirstComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <FirstComponent />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Components/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Component is alive</h1>
+    </div>
+  );
+};
+
+export default FirstComponent;
